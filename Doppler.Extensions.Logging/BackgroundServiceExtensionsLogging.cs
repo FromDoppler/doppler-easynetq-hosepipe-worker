8751d433959ae9dd0bfd7d788a5ea086735d5851
Rodrigo Pereyra Diaz <elsupergomez@gmail.com>
2024-02-19 18:17:28 -0300

    Revert "Use doppler extensions nuget for logging"

---
+++ Doppler.Extensions.Logging/BackgroundServiceExtensionsLogging.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Doppler.Extensions.Logging
+{
+    public static class BackgroundServiceExtensionsLogging
+    {
+        public static void LogServiceStart(this ILogger<BackgroundService> logger, BackgroundService service)
+        {
+            logger.LogInformation("Starting {Service} on {Host} running {OS} with runtime {Runtime}",
+                service.GetType().Name,
+                Environment.MachineName,
+                Environment.OSVersion,
+                Environment.Version);
+        }
+
+        public static void LogServiceStop(this ILogger<BackgroundService> logger, BackgroundService service)
+        {
+            logger.LogInformation("{Service} stoped on {Host}",
+                service.GetType().Name,
+                Environment.MachineName);
+        }
+    }
+}
